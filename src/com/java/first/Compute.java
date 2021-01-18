6909a27fbc5e8ec6088a3e004ee2fd59c3ba4889
김효린 <hyorin97@naver.com>
2021-01-18 21:18:12 +0900

    feat: Compute

---
+++ src/com/java/first/Compute.java
@@ -0,0 +1,28 @@
+package com.java.first;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 2개의 정수를 입력받아 곱과 몫 출력 
+ */
+public class Compute {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        
+        System.out.printf("곱= %d\n", a * b);
+        System.out.printf("몫= %d\n", a / b);
+
+
+	}
+
+}
