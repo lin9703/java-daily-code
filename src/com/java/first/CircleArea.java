fcb7c265839c4b8efe959f580a538c5221be4e19
김효린 <hyorin97@naver.com>
2021-01-18 21:17:36 +0900

    feat: CheckPoint

---
+++ src/com/java/first/CircleArea.java
@@ -0,0 +1,16 @@
+package com.java.first;
+
+/**
+ * 5cm인 원의 넓이 구하기 
+ */
+public class CircleArea {
+
+	public static void main(String[] args) {
+		int radius = 5;
+		double area = radius * radius * 3.14; 
+		
+		System.out.printf("반지름이 %dcm인 원의 넓이는 %.1fcm2입니다.\n", radius, area);
+		
+	}
+
+}
