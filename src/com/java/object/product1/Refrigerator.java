28ae825158086bcceb4afc4eb7cd3dfff9038dc4
김효린 <hyorin97@naver.com>
2021-01-27 21:08:50 +0900

    feat: add variables to TV and Refrigerator

--- src/com/java/object/product1/Refrigerator.java
+++ src/com/java/object/product1/Refrigerator.java
@@ -1,19 +1,29 @@
 package com.java.object.product1;
 
 public class Refrigerator extends Product {
+	private int capacity;
 
 	public Refrigerator() {
 		super();
 	}
 
-	public Refrigerator(int productNo, String productName, int price, int quantity) {
+	public Refrigerator(int productNo, String productName, int price, int quantity, int capacity) {
 		super(productNo, productName, price, quantity);
+		this.capacity = capacity;
+	}
+
+	public int getCapacity() {
+		return capacity;
+	}
+
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
 	}
 
 	@Override
 	public String toString() {
 		return "Refrigerator [productNo=" + getProductNo() + ", productName=" + getProductName()
-				+ ", price=" + getPrice() + ", quantity=" + getQuantity() + "]";
+				+ ", price=" + getPrice() + ", quantity=" + getQuantity() + ", capacity=" + capacity + "]";
 	}
 	
 }
--- src/com/java/object/product1/TV.java
+++ src/com/java/object/product1/TV.java
@@ -1,20 +1,41 @@
 package com.java.object.product1;
 
 public class TV extends Product {
+	private int inch;
+	private int displayType;
 
 	public TV() {
 		super();
 	}
 
-	public TV(int productNo, String productName, int price, int quantity) {
+	public TV(int productNo, String productName, int price, int quantity, int inch, int displayType) {
 		super(productNo, productName, price, quantity);
+		this.inch = inch;
+		this.displayType = displayType;
 	}
+
 	
+	public int getInch() {
+		return inch;
+	}
+
+	public void setInch(int inch) {
+		this.inch = inch;
+	}
+
+	public int getDisplayType() {
+		return displayType;
+	}
+
+	public void setDisplayType(int displayType) {
+		this.displayType = displayType;
+	}
 
 	@Override
 	public String toString() {
 		return "TV [productNo=" + getProductNo() + ", productName=" + getProductName()
-				+ ", price=" + getPrice() + ", quantity=" + getQuantity() + "]";
+				+ ", price=" + getPrice() + ", quantity=" + getQuantity()
+				+ ", inch=" + inch + ", displayType=" + displayType + "]";
 	}
 	
 
